8707cc6d0e396f5086a956223ef54746a0a34b67
jordanhoey <j.hoey@kainos.com>
2015-10-23 10:02:02 +0100

    creating redirect to chrisPage

--- src/mvc_course/controllers/WorldController.java
+++ src/mvc_course/controllers/WorldController.java
@@ -339,7 +339,7 @@ protected String doPost(Model m,HttpServletRequest request, HttpServletResponse 
 				} else if (category.equals("Finance")){
 					return "Finance";
 				}  else if (category.equals("Chris")){
-					return "Chris";
+					return "chrisPage";
 				} else {
 					return "logInError";
 				}
